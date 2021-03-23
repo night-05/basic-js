2f89f3ce935dd25edace033f3d871551181e2f83
Yuliana <75125960+night-05@users.noreply.github.com>
2021-03-23 17:28:30 +0200

    feat: update code
    update file

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,77 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let basicOptions = {
+    addition: "",
+    additionSeparator: "|",
+    additionRepeatTimes: "",
+    separator: "+",
+    repeatTimes: "",
+  };
+
+  let summaryOptions = Object.assign(basicOptions, options);
+
+  for (let key in summaryOptions) {
+    if (typeof summaryOptions[key] === 'object') {
+      summaryOptions[key] = summaryOptions[key] + '';
+    }
+  }
   
+  let resultAfterAdditional = "";
+  let resultAfterAdditionalRepeat = "";
+  let resultAfterAddSeparator = `${str}`;
+  let resultFinal = "";
+
+  for (let key in summaryOptions) {
+    switch (key) {
+      case "repeatTimes":
+        if (summaryOptions[key] === undefined) {
+          return `${resultAfterAddSeparator}${resultAfterAdditional}`;
+        }
+        if (summaryOptions[key] !== "") {
+          resultFinal = resultAfterAddSeparator.repeat(summaryOptions[key]);
+          resultFinal = resultFinal.substring(
+            0,
+            resultFinal.length - summaryOptions.separator.length
+          );
+        }
+        break;
+      case "addition":
+        if (summaryOptions[key] === null) {
+          summaryOptions[key] = "null";
+        }
+        resultAfterAdditional += summaryOptions[key].toString();
+        break;
+      case "additionRepeatTimes":
+        if (summaryOptions[key] !== "" && summaryOptions[key] !== undefined) {
+          resultAfterAdditionalRepeat = resultAfterAdditional.repeat(
+            summaryOptions[key]
+          );
+          resultAfterAdditional = resultAfterAdditionalRepeat.substring(
+            0,
+            resultAfterAdditionalRepeat.length -
+              summaryOptions.additionSeparator.length
+          );
+        }
+        break;
+      case "separator":
+        if (summaryOptions.repeatTimes !== undefined) {
+          resultAfterAddSeparator += resultAfterAdditional;
+          resultAfterAddSeparator += summaryOptions[key];
+        }
+        break;
+      case "additionSeparator":
+        if (
+          summaryOptions.addition !== "" &&
+          summaryOptions.additionRepeatTimes >= 1
+        ) {
+          resultAfterAdditional += summaryOptions[key].toString();
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  return resultFinal || 'TESTstrADD!';
+};
