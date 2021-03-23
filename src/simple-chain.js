eeb24415139e381cfe798c7589b10f6750550fae
Yuliana <75125960+night-05@users.noreply.github.com>
2021-03-23 17:30:11 +0200

    feat: update file
    add code

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  result: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.result.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.push(`( ${value} )`);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!Number.isNaN(position) && position <= this.result.length) {
+      this.result.splice(position - 1, 1);
+      return this;
+    } else {
+      this.result = [];
+      throw new Error();
+    }
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let finish = this.result;
+    this.result = [];
+
+    return finish.join("~~");
+  },
 };
 
 module.exports = chainMaker;
